068b102e9f0337d6db0df88397d66e138e4bb3e0
Ruben Silva <ruben.silva@decsis.pt>
2017-03-16 18:06:29 +0000

    Fixed metrics added database size
    still missing last backup size and duration...

--- scripts/prom_exporter.py
+++ scripts/prom_exporter.py
@@ -3,6 +3,7 @@ from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGIS
 import subprocess
 import time
 import random
+import re
 from  datetime import datetime
 
 def barman_list_servers():
@@ -24,11 +25,14 @@ def barman_check():
 	return status
 
 def backup_metrics():
-	size = GaugeMetricFamily('barman_backup_size_bytes', 'Last backup size in bytes', labels=['server_name'])
+	database_size = GaugeMetricFamily('barman_database_size_bytes', 'Database size in bytes', labels=['server_name'])
 	last_backup_age = GaugeMetricFamily('barman_last_backup_age_seconds', 'Last backup age', labels=['server_name'])
+	last_backup_size = GaugeMetricFamily('barman_last_backup_size_bytes', 'Last backup size in bytes', labels=['server_name'])
 	backup_window  = GaugeMetricFamily('barman_backup_window_seconds', 'Backup window covered by all existing backups', labels=['server_name'])
 	redundancy_actual  = GaugeMetricFamily('barman_current_redundancy', 'Number of existing backups', labels=['server_name'])
-redundancy_expected  = GaugeMetricFamily('barman_expected_redundancy', 'Number of expected backups as defined in config', labels=['server_name'])
+	redundancy_expected  = GaugeMetricFamily('barman_expected_redundancy', 'Number of expected backups as defined in config', labels=['server_name'])	
+	numbers_pattern = re.compile('\d+')
+subprocess.check_output("barman show-backup postgreslab1 last | grep 'with WALs' | cut -d':' -f 2")
 	for server in barman_list_servers():
 		try:
 			status_string = subprocess.check_output(["barman", "status", server])
@@ -36,32 +40,32 @@ redundancy_expected  = GaugeMetricFamily('barman_expected_redundancy', 'Number o
 			first_date = None
 			for line in status_string.split('\n'):
 				if "Current data size:" in  line:
-					size.add_metric([server],float(line.split(': ')[1].split()[0]) * 1024 *1024)
+					database_size.add_metric([server],float(line.split(': ')[1].split()[0]) * 1024 *1024)
 				if "available backups:" in  line:
 					redundancy_actual.add_metric([server],float(line.split(': ')[1]))
+				if "First available backup:" in  line:
+					try:
+						first_date = datetime.strptime( line.split(': ')[1], "%Y%m%dT%H%M%S" )
+					except ValueError as e:
+						True
 				if "Last available backup:" in  line:
 					try:
 						last_date = datetime.strptime( line.split(': ')[1], "%Y%m%dT%H%M%S" )
 						last_backup_age.add_metric([server],(datetime.utcnow()-last_date).total_seconds())
-					except ValueError as e:						
-						print "Value not valid"
-
-				if "First available backup:" in  line:
+						backup_window.add_metric([server],(last_date-first_date).total_seconds())							
+					except ValueError as e:												
+						True
+				if "Minimum redundancy requirements:" in  line:
 					try:
-						first_date = datetime.strptime( line.split(': ')[1], "%Y%m%dT%H%M%S" )
-						if last_date:
-							backup_window.add_metric([server],last_date - first_date)	
-						else:
-							backup_window.add_metric([server], 0)
-					except ValueError as e:
-						print "Value not valid"
+						redundancy_expected.add_metric([server], float(numbers_pattern.findall(line)[1]))
+					except:
+						True
 		except subprocess.CalledProcessError as e:
-			size.add_metric([server], -1)
-	return [size,redundancy_actual,backup_window,last_backup_age]
+			print "Unable to read status"
+	return [database_size,backup_window,last_backup_age,redundancy_actual,redundancy_expected]
 
 class CustomCollector(object):
-    def collect(self):
-        print barman_list_servers()        
+    def collect(self):        
         yield barman_check()
         for metric in backup_metrics():
         	yield metric
